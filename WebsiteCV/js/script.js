8f852f17d8af08cdd8d826817a524a2d1f15989c
Daniel Alvarez Najera <Daniel Alvarez Najera>
2017-01-25 22:06:03 -0600

    1.0 Skills

--- WebsiteCV/js/script.js
+++ WebsiteCV/js/script.js
@@ -2,3 +2,74 @@ $('.collapse').collapse("toggle")
 var shiftWindow = function() { scrollBy(0, -100) };
 if (location.hash) shiftWindow();
 window.addEventListener("hashchange", shiftWindow);
+
+function toggle_ph() {
+	var ph = document.getElementById("ph");
+	var pl = document.getElementById("pl");
+	var l = document.getElementById("l");
+	var Prs = document.getElementById("Prs");
+	var Pes = document.getElementById("Pes");
+	if(ph.style.display == "none") {
+    	ph.style.display = "block";
+		pl.style.display = "none";
+		l.style.display = "none";
+		Prs.style.display = "none";
+		Pes.style.display = "none";
+  	}
+}
+function toggle_pl() {
+	var ph = document.getElementById("ph");
+	var pl = document.getElementById("pl");
+	var l = document.getElementById("l");
+	var Prs = document.getElementById("Prs");
+	var Pes = document.getElementById("Pes");
+	if(pl.style.display == "none") {
+    	ph.style.display = "none";
+		pl.style.display = "block";
+		l.style.display = "none";
+		Prs.style.display = "none";
+		Pes.style.display = "none";
+  	}
+}
+function toggle_l() {
+	var ph = document.getElementById("ph");
+	var pl = document.getElementById("pl");
+	var l = document.getElementById("l");
+	var Prs = document.getElementById("Prs");
+	var Pes = document.getElementById("Pes");
+	if(l.style.display == "none") {
+    	ph.style.display = "none";
+		pl.style.display = "none";
+		l.style.display = "block";
+		Prs.style.display = "none";
+		Pes.style.display = "none";
+  	}
+} 
+function toggle_prs() {
+	var ph = document.getElementById("ph");
+	var pl = document.getElementById("pl");
+	var l = document.getElementById("l");
+	var Prs = document.getElementById("Prs");
+	var Pes = document.getElementById("Pes");
+	if(Prs.style.display == "none") {
+    	ph.style.display = "none";
+		pl.style.display = "none";
+		l.style.display = "none";
+		Prs.style.display = "block";
+		Pes.style.display = "none";
+  	}
+} 
+function toggle_pes() {
+	var ph = document.getElementById("ph");
+	var pl = document.getElementById("pl");
+	var l = document.getElementById("l");
+	var Prs = document.getElementById("Prs");
+	var Pes = document.getElementById("Pes");
+	if(Pes.style.display == "none") {
+    	ph.style.display = "none";
+		pl.style.display = "none";
+		l.style.display = "none";
+		Prs.style.display = "none";
+		Pes.style.display = "block";
+  	}
+}   
